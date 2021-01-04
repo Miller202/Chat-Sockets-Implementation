2dcf12d430f757bd4594035db92efe56fef0ed5f
Michael Miller <mullerg4michael@gmail.com>
2021-01-04 14:32:33 -0300

    Configuring the client and server connection;
    - Connection with sockets and threads.

--- client.py
+++ client.py
@@ -1,3 +1,16 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
+HOST = "localhost"
+PORT = 33000
+if not PORT:
+    PORT = 33000
+else:
+    PORT = int(PORT)
+
+BUFF_SIZE = 1024
+ADDRESS = (HOST, PORT)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDRESS)
+
--- server.py
+++ server.py
@@ -2,3 +2,32 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
+
+def accept_connections():
+    """Sets up handling for incoming clients."""
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s está online." % client_address)
+        client.send(bytes("Digite o seu nome:", "utf8"))
+        addresses[client] = client_address
+        Thread().start()
+
+
+clients = {}
+addresses = {}
+
+HOST = "localhost"
+PORT = 33000
+BUFF_SIZE = 1024
+ADDRESS = (HOST, PORT)
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDRESS)
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=accept_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+SERVER.close()
